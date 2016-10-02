caeedb4728257e5521350c73cc730c85b6767028
Thomas Miller <thomasmillergb@outlook.com>
2016-10-02 21:19:33 +0100

    add spring beans

--- src/main/java/coreStateMachine/StateDemo.java
+++ src/main/java/coreStateMachine/StateDemo.java
@@ -1,10 +1,11 @@
 package coreStateMachine;
 
 
-import coreStateMachine.state.Confirmed;
-import coreStateMachine.state.Firm;
-import coreStateMachine.state.Indicative;
 import coreStateMachine.state.State;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 /**
  * Created by Mingyi-PC on 02/10/2016.
@@ -12,17 +13,44 @@ import coreStateMachine.state.State;
  * @author Mingyi-PC
  */
 public class StateDemo {
-    public static void main(String[] args) {
-        StateContex contex = new StateContex();
+    private List<State> states;
+    private StateContex stateContex;
 
-        State indicative = new Indicative();
-        indicative.doAction(contex);
-        System.out.println(contex.getState().getCurrentState());
-        State firm = new Firm();
-        firm.doAction(contex);
-        System.out.println(contex.getState().getCurrentState());
-        State confirmed = new Confirmed();
-        confirmed.doAction(contex);
-        System.out.println(contex.getState().getCurrentState());
+    public StateDemo() {
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext springContex = new ClassPathXmlApplicationContext("classpath:/Beans.xml");
+        StateDemo state = (StateDemo) springContex.getBean("state");
+        state.start();
+//        State indicative = new Indicative();
+//        indicative.doAction(contex);
+//        System.out.println(contex.getState().getCurrentState());
+//        State firm = new Firm();
+//        firm.doAction(contex);
+//        System.out.println(contex.getState().getCurrentState());
+//        State confirmed = new Confirmed();
+//        confirmed.doAction(contex);
+//        System.out.println(contex.getState().getCurrentState());
+    }
+
+    public List<State> getStates() {
+        return states;
+    }
+
+    public void setStates(List<State> states) {
+        this.states = states;
+    }
+
+    public void setStateContex(StateContex stateContex) {
+        this.stateContex = stateContex;
+    }
+
+
+    public void start() {
+        for (State state : states) {
+            state.doAction(stateContex);
+            System.out.println(stateContex.getState().getCurrentState());
+        }
     }
 }
